d2151965ab0a92707d1da7f0d96344b9840f0395
Ethien Salinas <esalinas@creze.com>
2018-12-24 12:43:47 -0600

    [REFACTOR] resolvers to avoid mix with fake data

--- resolvers.js
+++ resolvers.js
@@ -1,23 +1,4 @@
-// Some fake data
-const authors = [
-  {
-    id: "1",
-    info: {
-      name: "Joe Kelly",
-      age: 32,
-      gender: "M",
-    }
-  },
-  {
-    id: "2",
-    info: {
-      name: "Mary Jane",
-      age: 27,
-      gender: "F",
-    }
-  }
-];
-
+const authors = require('./authors');
 // GraphQL: Resolvers
 const resolvers = {
   Query: {
